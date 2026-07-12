eb1597f2d340929e7f1985f0a436f4a9fad097a0
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2026-07-12 21:29:39 +1000

    chore: remove team page from site navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import Home from './home';
-import Team from './team'
 import Podcast from './podcast'
 import Covidsations from './covidsations'
 import Blog from './blog'
 import BlogPage from './blogpage'
 
-export {Home, Team, Podcast, Covidsations, Blog, BlogPage}
+export {Home, Podcast, Covidsations, Blog, BlogPage}
